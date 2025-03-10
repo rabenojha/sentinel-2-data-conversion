fba57de4e6ec715b54ed218d03a7b8effa55913a
rabenojha <rabenojha@gmail.com>
2025-03-10 01:24:10 +0100

    add notebook to convert data from SAFE format to Zarr

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# Username and Password for Copernicus Data Space Ecosystem
+
+# If you do not have credentials already, please register at https://dataspace.copernicus.eu
+
+# Credentials should be inside quotation marks. 
+
+
+username = "************"
+password = "************"

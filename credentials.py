cce9abc1e949593e83da0e6d09601fe3df87ab99
rabenojha <rabenojha@gmail.com>
2025-03-10 23:53:25 +0100

    move credentials section

--- credentials.py
+++ credentials.py
@@ -2,9 +2,8 @@
 
 # If you do not have credentials already, please register at https://dataspace.copernicus.eu
 
+
 # Credentials should be inside quotation marks. 
-
-
 
 # username = "************"
 # password = "************"
